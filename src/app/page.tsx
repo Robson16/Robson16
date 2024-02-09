89757a3b0ea852bca9209914ef9828fe10f2b066
Robson H. Rodrigues <robhenrod@gmail.com>
2024-02-09 12:17:58 -0300

    fix: for mobiles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,19 @@ import {
   AiFillLinkedin,
   AiOutlineMail,
 } from 'react-icons/ai'
-import { BsFillBuildingFill } from 'react-icons/bs'
 import { FaMapMarkerAlt } from 'react-icons/fa'
 
 export default function Home() {
   return (
     <main>
       <div className="container max-w-screen-xl mx-auto py-28 px-4">
-        <div className="columns-2 flex">
-          <div className="flex flex-col justify-center flex-1">
-            <h1 className="text-5xl font-bold mb-4">Robson H. Rodrigues</h1>
+        <div className="columns-2 flex flex-col md:flex-row">
+          <div className="flex flex-col justify-center items-center md:items-start flex-1">
+            <h1 className="text-5xl font-bold mb-4 text-center md:text-left">
+              Robson H. Rodrigues
+            </h1>
             <h2 className="text-2xl font-medium">Desenvolvedor Web</h2>
             <ul className="my-10">
-              <li className="group flex items-center my-2">
-                <BsFillBuildingFill
-                  size={16}
-                  className="text-gray-500 group-hover:text-teal-600 mr-2"
-                />
-                <a href="https://www.agenciabeb.com.br/" target="_blank">
-                  Agência B&B
-                </a>
-              </li>
               <li className="group flex items-center my-2">
                 <AiOutlineMail
                   size={16}
@@ -72,13 +64,14 @@ export default function Home() {
             </ul>
           </div>
           <div className="flex flex-col justify-center flex-1">
-            <figure className="mx-auto rounded-full border border-[20px] border-zinc-950">
+            <figure className="mx-auto rounded-full border-[20px] border-zinc-950">
               <Image
                 src="https://github.com/Robson16.png"
+                priority={true}
                 alt="Picture of the author"
                 width={360}
                 height={360}
-                className="mx-auto rounded-full border border-[20px] border-zinc-900"
+                className="mx-auto rounded-full border-[20px] border-zinc-900"
               />
             </figure>
           </div>
