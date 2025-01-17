b95f45723b90cf05e6a02175b45cb7925fec949c
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-01-17 14:48:14 -0300

    feat: skills api routes

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,6 +1,26 @@
 import { CircularProgress, Progress } from '@nextui-org/progress'
 
-export default function SkillsSections() {
+export type Skill = {
+  name: string
+  value: number
+}
+
+export type SkillResponse = {
+  technical: Skill[]
+  professional: Skill[]
+}
+
+async function getSkills(): Promise<SkillResponse> {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || ''
+  const res = await fetch(`${apiUrl}/api/skills`)
+
+  if (!res.ok) throw new Error('Falha ao buscar habilidades')
+  return res.json()
+}
+
+export default async function SkillsSections() {
+  const skills = await getSkills()
+
   return (
     <section
       id="skills"
@@ -11,152 +31,48 @@ export default function SkillsSections() {
           <h3 className="mb-12 text-center text-3xl font-medium">
             Habilidades Técnicas
           </h3>
-          <Progress
-            label="JavaScript"
-            classNames={{
-              base: 'w-full',
-              track: 'drop-shadow-md border border-default',
-              indicator: 'bg-teal-600',
-              label: 'tracking-wider text-default-600',
-              value: 'text-foreground/60',
-            }}
-            size="md"
-            value={90}
-          />
-          <Progress
-            label="TypeScript"
-            classNames={{
-              base: 'w-full',
-              track: 'drop-shadow-md border border-default',
-              indicator: 'bg-teal-600',
-              label: 'tracking-wider text-default-600',
-              value: 'text-foreground/60',
-            }}
-            size="md"
-            value={80}
-          />
-          <Progress
-            label="ReactJS"
-            classNames={{
-              base: 'w-full',
-              track: 'drop-shadow-md border border-default',
-              indicator: 'bg-teal-600',
-              label: 'tracking-wider text-default-600',
-              value: 'text-foreground/60',
-            }}
-            size="md"
-            value={70}
-          />
-          <Progress
-            label="NodeJS"
-            classNames={{
-              base: 'w-full',
-              track: 'drop-shadow-md border border-default',
-              indicator: 'bg-teal-600',
-              label: 'tracking-wider text-default-600',
-              value: 'text-foreground/60',
-            }}
-            size="md"
-            value={75}
-          />
-          <Progress
-            label="PHP"
-            classNames={{
-              base: 'w-full',
-              track: 'drop-shadow-md border border-default',
-              indicator: 'bg-teal-600',
-              label: 'tracking-wider text-default-600',
-              value: 'text-foreground/60',
-            }}
-            size="md"
-            value={80}
-          />
-          <Progress
-            label="WordPress"
-            classNames={{
-              base: 'w-full',
-              track: 'drop-shadow-md border border-default',
-              indicator: 'bg-teal-600',
-              label: 'tracking-wider text-default-600',
-              value: 'text-foreground/60',
-            }}
-            size="md"
-            value={90}
-          />
-          <Progress
-            label="Java"
-            classNames={{
-              base: 'w-full',
-              track: 'drop-shadow-md border border-default',
-              indicator: 'bg-teal-600',
-              label: 'tracking-wider text-default-600',
-              value: 'text-foreground/60',
-            }}
-            size="md"
-            value={40}
-          />
+          {skills.technical.map(
+            (skill: { name: string; value: number }, index: number) => (
+              <Progress
+                key={index}
+                label={skill.name}
+                classNames={{
+                  base: 'w-full',
+                  track: 'drop-shadow-md border border-default',
+                  indicator: 'bg-teal-600',
+                  label: 'tracking-wider text-default-600',
+                  value: 'text-foreground/60',
+                }}
+                size="md"
+                value={skill.value}
+              />
+            ),
+          )}
         </div>
         <div className="flex flex-1 flex-col gap-4">
           <h3 className="mb-12 text-center text-3xl font-medium">
             Habilidades Profissionais
           </h3>
           <div className="grid grid-cols-2 items-start justify-center gap-8">
-            <CircularProgress
-              classNames={{
-                base: 'max-w-full text-center',
-                svg: 'w-36 h-36 drop-shadow-md',
-                indicator: 'stroke-teal-600',
-                track: 'drop-shadow-md border border-default',
-                value: 'text-3xl font-semibold text-white',
-              }}
-              formatOptions={{ style: 'unit', unit: 'percent' }}
-              label="Resolução de Problemas"
-              showValueLabel={true}
-              strokeWidth={2}
-              value={90}
-            />
-            <CircularProgress
-              classNames={{
-                base: 'max-w-full text-center',
-                svg: 'w-36 h-36 drop-shadow-md',
-                indicator: 'stroke-teal-600',
-                track: 'drop-shadow-md border border-default',
-                value: 'text-3xl font-semibold text-white',
-              }}
-              formatOptions={{ style: 'unit', unit: 'percent' }}
-              label="Trabalho em Equipe"
-              showValueLabel={true}
-              strokeWidth={2}
-              value={80}
-            />
-            <CircularProgress
-              classNames={{
-                base: 'max-w-full text-center',
-                svg: 'w-36 h-36 drop-shadow-md',
-                indicator: 'stroke-teal-600',
-                track: 'drop-shadow-md border border-default',
-                value: 'text-3xl font-semibold text-white',
-              }}
-              formatOptions={{ style: 'unit', unit: 'percent' }}
-              label="Gerenciamento de Projetos"
-              showValueLabel={true}
-              strokeWidth={2}
-              value={86}
-            />
-            <CircularProgress
-              classNames={{
-                base: 'max-w-full text-center',
-                svg: 'w-36 h-36 drop-shadow-md',
-                indicator: 'stroke-teal-600',
-                track: 'drop-shadow-md border border-default',
-                value: 'text-3xl font-semibold text-white',
-              }}
-              formatOptions={{ style: 'unit', unit: 'percent' }}
-              label="Autodidatismo"
-              showValueLabel={true}
-              strokeWidth={2}
-              value={90}
-            />
+            {skills.professional.map(
+              (skill: { name: string; value: number }, index: number) => (
+                <CircularProgress
+                  key={index}
+                  classNames={{
+                    base: 'max-w-full text-center',
+                    svg: 'w-36 h-36 drop-shadow-md',
+                    indicator: 'stroke-teal-600',
+                    track: 'drop-shadow-md border border-default',
+                    value: 'text-3xl font-semibold text-white',
+                  }}
+                  formatOptions={{ style: 'unit', unit: 'percent' }}
+                  label={skill.name}
+                  showValueLabel={true}
+                  strokeWidth={2}
+                  value={skill.value}
+                />
+              ),
+            )}
           </div>
         </div>
       </div>
